e77c3f58e732e10fcb1f701e1cfb5e73f1df723c
Dawid <302052@uwr.edu.pl>
2019-04-08 14:51:09 +0200

    SI

--- SI - sztuczna inteligencja/Ćwiczenia 02/Zad 2/compare.py
+++ SI - sztuczna inteligencja/Ćwiczenia 02/Zad 2/compare.py
@@ -1,9 +1,14 @@
 # run both programs
 # import zad2
-import rand_sent
+# import rand_sent
 
 RANDOM = []
 VALID = []
+TADEK = []
+
+with open('panTadeuszPoprawny_out.txt') as f:
+    for line in f:
+        TADEK.append(line.strip())
 
 with open('random_output.txt') as f:
     for line in f:
@@ -17,8 +22,25 @@ with open('valid_output.txt') as f:
 all_lines = len(RANDOM)
 diff = 0
 for i in range(all_lines):
-    if RANDOM[i] != VALID[i]:
+    if RANDOM[i] != TADEK[i]:
         diff += 1
 
+print('Random with original')
+print((all_lines - diff) / all_lines * 100, ' % match.\n')
 
-print((all_lines - diff) / all_lines * 100, ' % match.')
+diff = 0
+for i in range(all_lines):
+    if VALID[i] != TADEK[i]:
+        diff += 1
+
+print('Max squares with original')
+print((all_lines - diff) / all_lines * 100, ' % match.\n')
+
+
+diff = 0
+for i in range(all_lines):
+    if VALID[i] != RANDOM[i]:
+        diff += 1
+
+print('Random with max squares')
+print((all_lines - diff) / all_lines * 100, ' % match.\n')
